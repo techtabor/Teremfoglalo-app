25f33c12c139ed914cf48caad72f32f0f534d5da
vtamas2000 <veres454@gmail.com>
2016-12-01 19:52:46 +0100

    Better 404 response; header added

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 var http = require('http');
 var express = require('express');
+var fs = require('fs');
 var app = express();
 
 var main = require('./routes/main.js');
@@ -11,10 +12,14 @@ app.use(express.static(__dirname));
 app.use('/', main);
 
 //Send 404 response
-app.use(function(req, res){
-    res.status(404).send("Sorry this doesn't exist");
+app.use(function(req, res, err){
+    if(err){
+        fs.createReadStream('./views/Error.ejs').pipe(res);
+        console.log("Error 404: Someone requested a non-existing page");
+    }
 });
 
+//create http server
 http.createServer(app).listen(3000);
 
 console.log("Server running...");
