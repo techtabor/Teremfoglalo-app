3e699f3ca00a0b7ec6d44d0ed2a1dc587d3d2386
vtamas2000 <veres454@gmail.com>
2016-12-01 15:18:59 +0100

    404 response added

--- app.js
+++ app.js
@@ -4,10 +4,16 @@ var app = express();
 
 var main = require('./routes/main.js');
 
+//set the view engine to ejs
 app.set('view engine', 'ejs');
 app.use(express.static(__dirname));
 
 app.use('/', main);
+
+//Send 404 response
+app.use(function(req, res){
+    res.status(404).send("Sorry this doesn't exist");
+});
 
 http.createServer(app).listen(3000);
 
--- routes/main.js
+++ routes/main.js
@@ -2,7 +2,8 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/', function(req, res){
-    res.render('../views/Main.ejs')
+    res.render('../views/Main.ejs');
+    console.log("Request jött a homepage-re");
 });
 
 module.exports = router;
