4e234f5fdd141898fba4e47f459e57c06234a9e4
vtamas2000 <veres454@gmail.com>
2016-12-21 12:35:24 +0100

    Submit form added

--- app.js
+++ app.js
@@ -2,22 +2,32 @@ var http = require('http');
 var express = require('express');
 var fs = require('fs');
 var app = express();
+var bodyParser = require('body-parser');
 
 var main = require('./routes/main.js');
+//var orak = require('./Data/orak.json');
 
 //set the view engine to ejs
 app.set('view engine', 'ejs');
 app.use(express.static(__dirname));
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use('/', main);
 
 //Send 404 response
-app.use(function(req, res, err){
+/*app.use(function(req, res, err){
     if(err){
         fs.createReadStream('./views/Error.ejs').pipe(res);
         console.log("Error 404: Someone requested a non-existing page");
-        console.log(err);
+        //console.log(err);
     }
+});*/
+
+app.post('/submit', function(req, res){
+  console.log("Someone submitted a form");
+  var name = req.body.name;
+  //var id = req.body.name;
+  res.send(name);
 });
 
 //create http server
--- routes/main.js
+++ routes/main.js
@@ -2,13 +2,29 @@ var express = require('express');
 var router = express.Router();
 
 var orak = require('../Data/orak.json');
+var hossz = orak.osszorak.length;
+var oraarray = new Array(hossz);
+for (var i = 0; i < oraarray.length; i++){
+    oraarray[i] = new Array(6);
+};
+
+for (var i = 0; i < hossz; i++){
+    for (var j = 0; j < 6; j++){
+      oraarray[i][j] = orak.osszorak[i].hetiorak[j];
+    }
+}
 
 router.get('/', function(req, res){
     res.render('../views/Main.ejs',
     {
-      orak: orak
+      oraarray: oraarray,
+      orak: orak,
+      hossz: hossz
     });
     console.log("Request jött a homepage-re");
 });
 
+router.get('/submit')
+
 module.exports = router;
+//module.exports = oraarray;
