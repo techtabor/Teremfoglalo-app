f4e39abbcf9a6573b679dcc62cdc2c83ef3e95f1
vtamas2000 <veres454@gmail.com>
2017-02-27 22:52:21 +0100

    Színes órarend elkezdve

--- routes/newmain.js
+++ routes/newmain.js
@@ -1,8 +1,19 @@
 var express = require('express');
 var router = express.Router();
+var orak = require('../Data/OraID.json');
+
+var hossz = orak.minden.length;
+var oraarray = new Array(hossz);
+
+for (var i = 0; i < hossz; i++){
+	oraarray[i] = orak.minden[i];
+}
 
 router.get('/', function(req, res){
-    res.render('../views/NewMain.ejs');
+    res.render('../views/NewMain.ejs',
+    	{
+    		oraarray: oraarray
+    	});
     console.log("Request jött a homepage-re");
 });
 
