ff0744d1159ea3d08626f7f96109e795560d37f0
vtamas2000 <veres454@gmail.com>
2017-06-05 13:17:21 +0200

    Login started

--- app.js
+++ app.js
@@ -5,12 +5,14 @@ var app = express();
 var bodyParser = require('body-parser');
 
 var main = require('./routes/main.js');
+var login = require('./routes/login.js');
 
 app.set('view engine', 'ejs');
 app.use(express.static(__dirname));
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use('/', main);
+app.use('/login', login);
 
 //create http server
 http.createServer(app).listen(3000);
